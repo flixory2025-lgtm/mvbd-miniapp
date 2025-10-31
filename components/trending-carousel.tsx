7efbbaf5b8ea9a33a996d31f2f0a5e638dde07cb
flixory2025-lgtm <flixory2025@gmail.com>
2025-10-31 10:51:28 +0600

    Update trending-carousel.tsx

--- components/trending-carousel.tsx
+++ components/trending-carousel.tsx
@@ -4,7 +4,7 @@ import { useState, useEffect } from "react"
 import { ChevronLeft, ChevronRight } from "lucide-react"
 import { movies } from "@/lib/movie-data"
 
-const trendingIds = [898, 913, 917, 923, 926, 928, 936, 861,]
+const trendingIds = [898, 913, 959, 962, 961, 928, 960, 956,]
 
 interface TrendingCarouselProps {
   onMovieClick: (movie: (typeof movies)[0]) => void

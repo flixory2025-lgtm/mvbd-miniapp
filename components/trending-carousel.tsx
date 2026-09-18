9a97b539f79ca6d06057c4e011196769789851fb
flixory2025-lgtm <flixory2025@gmail.com>
2026-09-18 20:34:45 +0600

    Update trending-carousel.tsx

--- components/trending-carousel.tsx
+++ components/trending-carousel.tsx
@@ -83,7 +83,7 @@ export default function TrendingCarousel({ onMovieClick }: TrendingCarouselProps
           <div className="w-full h-96 bg-gradient-to-b from-black/50 via-black/80 to-black rounded-b-3xl" />
         </div>
         <img
-          src="https://i.postimg.cc/0NVvbFgH/17311-removebg-preview.png"
+          src="https://i.postimg.cc/dQkXgjR7/17818-removebg-preview.png"
           alt="MoviesVerseBD Logo"
           className="relative z-20 w-72 h-72 object-contain"
         />
